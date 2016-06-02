0ee2831300e8403785737150526532a9b284df8e
kmahesh-sailfish <maheshvrsecit@gamil.com>
2016-06-02 18:11:43 +0530

    commit the code

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var express = require('express');
-
+var mysql =require('mysql');
+var path     = require('path');
+ var   bodyParser = require('body-parser');
 var app = express();
 
+
+var connection = mysql.createConnection({
+ connectionLimit :20,
+ host:'localhost',
+ user:'root',
+ password:'Rubhu@111213',
+ database:'test'
+ });
+app.use(bodyParser.urlencoded({ extended: true })); //support x-www-form-urlencoded
+app.use(bodyParser.json());
 app.get('/',function(req,res){
   res.send('welcome to Api exprees1..');
 
@@ -17,6 +29,36 @@ app.get('/sample',function(req,res){
         "RollNo":"007"
       });
 });
+app.get('/display',function(req,res,next){
+    var query=connection.query('select * from files',function(err,rows){
+        res.send(rows);
+    })
+})
+
+app.post('/inserid',function(req,res,next){
+    try{
+        var query = "INSERT INTO files (file_data) VALUES  " +
+            "('" + req.body['file_data']+"')";
+        connection.query(query, function (err, rows) {
+
+            if (err) {
+                console.log(err);
+                res.status(500).send('500 Error : ' + err);
+            }
+            else {
+
+                res.status(200).json(rows);
+            }
+        });
+    }
+    catch(err)
+    {
+        console.log("error on the body"+err);
+    }
+
+})
+
+
 
 // development error handler
 // will print stacktrace
