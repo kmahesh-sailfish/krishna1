0fad1a5483e0e919d70d016c4f698bd4f6372881
kmahesh-sailfish <maheshvrsecit@gamil.com>
2016-06-03 08:52:42 +0530

    commit the code

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ var path     = require('path');
  var   bodyParser = require('body-parser');
 var app = express();
 
-
-var connection = mysql.createConnection({
- connectionLimit :20,
- host:'localhost',
- user:'root',
- password:'Rubhu@111213',
- database:'test'
- });
+var pool=mysql.createPool({
+    host:'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user:'eiskd7d5t00o1shv',
+    password:'fkvdijztrdb8gdcg',
+    database:'ccegfa99xf80kp53',
+    ConnectionLimit:20
+});
+//var connection = mysql.createConnection({
+// connectionLimit :20,
+// host:'localhost',
+// user:'root',
+/// password:'Rubhu@111213',
+// database:'test'
+ //});
 app.use(bodyParser.urlencoded({ extended: true })); //support x-www-form-urlencoded
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+
+});
 app.get('/',function(req,res){
   res.send('welcome to Api exprees1..');
 
@@ -30,26 +42,42 @@ app.get('/sample',function(req,res){
       });
 });
 app.get('/display',function(req,res,next){
-    var query=connection.query('select * from files',function(err,rows){
-        res.send(rows);
-    })
-})
-
-app.post('/inserid',function(req,res,next){
-    try{
-        var query = "INSERT INTO files (file_data) VALUES  " +
-            "('" + req.body['file_data']+"')";
-        connection.query(query, function (err, rows) {
-
-            if (err) {
+    pool.getConnection(function(err,connection){
+        var query='select * from files';
+        connection.query(query,function(err,rows){
+            connection.release();
+            if(err){
                 console.log(err);
                 res.status(500).send('500 Error : ' + err);
             }
-            else {
-
+            else
+            {
                 res.status(200).json(rows);
             }
         });
+    });
+
+});
+
+app.post('/inserid',function(req,res,next){
+    try{
+
+        pool.getConnections(function(err,connection){
+            var query = "INSERT INTO files (file_data) VALUES  " +
+                "('" + req.body['file_data']+"')";
+                    connection.query(query,function(err,rows){
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('500 Error : ' + err);
+                }
+                else {
+
+                    res.status(200).json(rows);
+
+                }
+            })
+        })
+
     }
     catch(err)
     {
